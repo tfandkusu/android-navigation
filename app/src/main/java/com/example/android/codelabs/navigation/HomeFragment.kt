b0f24073c11fe31a8a0121d7a4d95648c7cb7c3a
Haruhiko Takada <haruhiko.takada@gmail.com>
2020-02-18 23:15:20 +0900

    Finish step 5

--- app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
+++ app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
@@ -18,7 +18,11 @@ package com.example.android.codelabs.navigation
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
 
 /**
  * Fragment used to show how to navigate to another destination
@@ -36,11 +40,13 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //TODO STEP 5 - Set an OnClickListener, using Navigation.createNavigateOnClickListener()
-//        val button = view.findViewById<Button>(R.id.navigate_destination_button)
-//        button?.setOnClickListener {
-//            findNavController().navigate(R.id.flow_step_one_dest, null)
-//        }
+        // STEP 5 - Set an OnClickListener, using Navigation.createNavigateOnClickListener()
+        val button = view.findViewById<Button>(R.id.navigate_destination_button)
+        button?.setOnClickListener {
+
+            findNavController().navigate(R.id.flow_step_one_dest
+                    /*ここではnavigation.xmlで定義したFragmentのIDまたはActionのIDを指定できる*/, null)
+        }
         //TODO END STEP 5
 
         //TODO STEP 6 - Set NavOptions
